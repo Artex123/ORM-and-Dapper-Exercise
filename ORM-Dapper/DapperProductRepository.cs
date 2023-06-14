fa17ed8dc3e91b02c1b861c6d278a056213621e6
Ben Zellers <zellersdenver@gmail.com>
2023-06-14 14:45:24 -0400

    All Exercises complete

---
+++ ORM-Dapper/DapperProductRepository.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public class DapperProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+        public DapperProductRepository(IDbConnection conn) 
+        {
+            _conn = conn;
+        }
+        public void CreateProduct(string name, double price, int categoryID)
+        {
+            _conn.Execute("INSERT INTO products (Name, Price, CategoryID) VALUES (@name, @price, @categoryid);",
+                new {name = name, price = price, categoryid = categoryID});
+        }
+
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM products;");
+        }
+
+        public Product GetProduct(int ID)
+        {
+            return _conn.QuerySingle<Product>("SELECT * FROM products WHERE ProductID = @ID;",
+                new {ID});
+        }
+        public void DeleteProduct(int ID)
+        {
+            _conn.Execute("DELETE FROM sales WHERE ProductID = @id;", new {ID = ID});
+            _conn.Execute("DELETE FROM reviews WHERE ProductID = @id;", new { ID = ID });
+            _conn.Execute("DELETE FROM products WHERE ProductID = @id;", new { ID = ID });
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name, price = @price, " +
+                "CategoryID = @categoryID, OnSale = @onsale, " +
+                "StockLevel = @stocklevel WHERE ProductID = @id",
+                new {name = product.Name, price = product.Price, 
+                    categoryID = product.CategoryID, onsale = product.OnSale, 
+                    stocklevel = product.StockLevel, id = product.ProductID});
+            
+        }
+    }
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -16,16 +16,46 @@ namespace ORM_Dapper
             string connString = config.GetConnectionString("DefaultConnection");
 
             IDbConnection conn = new MySqlConnection(connString);
-            var departmentRepo = new DapperDepartmentRepository(conn);
-            departmentRepo.InsertDepartment("Ben's New Department");
-            var departments = departmentRepo.GetAllDepartments();
-            foreach(var department in departments)
-            {
-                Console.WriteLine(department.DepartmentID);
-                Console.WriteLine(department.Name);
-                Console.WriteLine();
-                Console.WriteLine();
+            #region Department Area
+            //var departmentRepo = new DapperDepartmentRepository(conn);
+            // departmentRepo.InsertDepartment("Ben's New Department");
+            //var departments = departmentRepo.GetAllDepartments();
+            //foreach(var department in departments)
+            // {
+            // Console.WriteLine(department.DepartmentID);
+            // Console.WriteLine(department.Name);
+            // Console.WriteLine();
+            // Console.WriteLine();
 
+            // }
+            #endregion
+
+            var productRepository = new DapperProductRepository(conn);
+
+            //var productToUpdate = productRepository.GetProduct(941);
+            //productToUpdate.OnSale = 0;
+            //productToUpdate.Name = "Seeing if this worked!";
+            //productToUpdate.Price = 300;
+            //productToUpdate.CategoryID = 1;
+            //productToUpdate.StockLevel = 1;
+
+
+            //productRepository.UpdateProduct(productToUpdate);
+
+            productRepository.DeleteProduct(940);
+            productRepository.DeleteProduct(941);
+            productRepository.DeleteProduct(942);
+
+            var products = productRepository.GetAllProducts();
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.ProductID);
+                Console.WriteLine(product.StockLevel);
+                Console.WriteLine(product.Price);
+                Console.WriteLine(product.OnSale);
+                Console.WriteLine();
+                Console.WriteLine();
             }
         }
     }
